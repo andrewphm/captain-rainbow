9bcad42bfe70b2c14dee87723197d50b20be4fac
Andrew Pham <andrew.pham@shopify.com>
2022-09-27 10:14:52 -0400

    Add CRUD functions for list operability

---
+++ checklist.py
@@ -0,0 +1,45 @@
+checklist = list()
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+
+# READ
+def read(index):
+    return checklist[index]
+
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+
+# DELETE
+def delete(index):
+    checklist.pop(index)
+
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(f"{index} {list_item}")
+        index += 1
+
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    delete(1)
+
+    print(read(0))
+
+    list_all_items()
+
+
+test()
